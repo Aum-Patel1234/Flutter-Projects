c5cfe363f598cbc20a64d7ce66e2908f0403b930
Aum Patel <aumpatel131@gmail.com>
2024-08-02 20:11:21 +0530

    fixed some things ..

--- whatsapp_clone/lib/firebase_options.dart
+++ whatsapp_clone/lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -40,47 +41,47 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyBlngHaY-tgaKLuLuanegMttnJJkrk8CVo',
-    appId: '1:160921869350:web:2419a3ca8318eedb0f5ae7',
-    messagingSenderId: '160921869350',
-    projectId: 'flutter-chat-app-151e5',
-    authDomain: 'flutter-chat-app-151e5.firebaseapp.com',
-    storageBucket: 'flutter-chat-app-151e5.appspot.com',
+  static FirebaseOptions web = FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY_WEB']!,
+    appId: dotenv.env['FIREBASE_APP_ID_WEB']!,
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_GLOBAL']!,
+    projectId: dotenv.env['FIREBASE_PROJECT_ID_WEB']!,
+    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN_WEB']!,
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_WEB']!,
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAsZbLkisj8BkSsrCS1HFQtSy71zrVQnik',
-    appId: '1:160921869350:android:db7d28691b8f893c0f5ae7',
-    messagingSenderId: '160921869350',
-    projectId: 'flutter-chat-app-151e5',
-    storageBucket: 'flutter-chat-app-151e5.appspot.com',
+  static FirebaseOptions android = FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY_ANDROID']!,
+    appId: dotenv.env['FIREBASE_APP_ID_ANDROID']!,
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_GLOBAL']!,
+    projectId: dotenv.env['FIREBASE_PROJECT_ID_ANDROID']!,
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_ANDROID']!,
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyDNhKW1OG1GFAYNwqHBS1VRzPxMKNpOtos',
-    appId: '1:160921869350:ios:09e518c5c45bebc70f5ae7',
-    messagingSenderId: '160921869350',
-    projectId: 'flutter-chat-app-151e5',
-    storageBucket: 'flutter-chat-app-151e5.appspot.com',
-    iosBundleId: 'com.aumpatelwhatsappclone.whatsappClone',
+  static FirebaseOptions ios = FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY_IOS']!,
+    appId: dotenv.env['FIREBASE_APP_ID_IOS']!,
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_GLOBAL']!,
+    projectId: dotenv.env['FIREBASE_PROJECT_ID_IOS']!,
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_IOS']!,
+    iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID']!,
   );
 
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyDNhKW1OG1GFAYNwqHBS1VRzPxMKNpOtos',
-    appId: '1:160921869350:ios:09e518c5c45bebc70f5ae7',
-    messagingSenderId: '160921869350',
-    projectId: 'flutter-chat-app-151e5',
-    storageBucket: 'flutter-chat-app-151e5.appspot.com',
-    iosBundleId: 'com.aumpatelwhatsappclone.whatsappClone',
+  static FirebaseOptions macos = FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY_MACOS']!,
+    appId: dotenv.env['FIREBASE_APP_ID_MACOS']!,
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_GLOBAL']!,
+    projectId: dotenv.env['FIREBASE_PROJECT_ID_MACOS']!,
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_MACOS']!,
+    iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID_MACOS']!,
   );
 
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyBlngHaY-tgaKLuLuanegMttnJJkrk8CVo',
-    appId: '1:160921869350:web:211fce27b9268fd00f5ae7',
-    messagingSenderId: '160921869350',
-    projectId: 'flutter-chat-app-151e5',
-    authDomain: 'flutter-chat-app-151e5.firebaseapp.com',
-    storageBucket: 'flutter-chat-app-151e5.appspot.com',
+  static FirebaseOptions windows = FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY_WINDOWS']!,
+    appId: dotenv.env['FIREBASE_APP_ID_WINDOWS']!,
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_GLOBAL']!,
+    projectId: dotenv.env['FIREBASE_PROJECT_ID_WINDOWS']!,
+    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN_WINDOWS']!,
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_WINDOWS']!,
   );
 }
